0bf66fa7697c51e7f7fdbebe8294944d706be5dc
Fabio Carriel Fonseca <ffonseca@modulusdata.com>
2018-03-28 19:49:03 -0400

    renaming

--- src/CircularSuffixArray.java
+++ src/CircularSuffixArray.java
@@ -19,7 +19,24 @@
  * 
  * 
  */
-public class CircularArraySuffix {
+public class CircularSuffixArray {
+	
+	private String s;
+	
+	// circular suffix array of s
+	public CircularSuffixArray(String s) {
+		 
+	 }
+	
+	// length of s
+	public int length() {
+		return s.length();
+	}
+	
+	// returns index of ith sorted suffix
+	public int index(int i) {
+		return 0;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
