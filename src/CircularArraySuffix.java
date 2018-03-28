30de00ee4fde50a87c8f48aa903ce1b8b741212f
Fabio Carriel Fonseca <ffonseca@modulusdata.com>
2018-03-28 19:43:08 -0400

    initializes project with CircularSuffix

---
+++ src/CircularArraySuffix.java
@@ -0,0 +1,29 @@
+
+
+/**
+ * 
+ * @author ffonseca
+ * 
+ * Since as of java 1.7, the substring function no longer takes constant time, the mains insight 
+ * for this class is how to have a data structure that will have the same String reference of the original 
+ * String provided in the constructor. The idea is to have a private inner class (CirucularSufix), that will
+ * implement the Comparable interface. This inner class will have a String that will be the same as the
+ * outer class and an integer (index), that will indicate the first letter of the circular sufix.
+ * This is enough to identify the entire circular suffix.
+ * 
+ *  Tha main inspiration for this is the old substring implementation java that was shown in the lectures notes 
+ * 
+ * A more update version of the checklist can be found in the Princeton course website:
+ * http://www.cs.princeton.edu/courses/archive/spring18/cos226/assignments.php. It has a good hint on 	
+ * the checklist for how to sort the CircularArray
+ * 
+ * 
+ */
+public class CircularArraySuffix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
