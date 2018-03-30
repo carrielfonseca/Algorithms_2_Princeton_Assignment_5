a3c596dcc7a037ffe47a03d288836b69b643890c
Fabio Carriel Fonseca <ffonseca@modulusdata.com>
2018-03-29 22:35:15 -0400

    corrects transform() and creates private method

--- src/BurrowsWheeler.java
+++ src/BurrowsWheeler.java
@@ -6,15 +6,21 @@ public class BurrowsWheeler {
 	
 	
 	// apply Burrows-Wheeler transform, reading from standard input and writing to standard output
-    public static void transform() {
+    public static void transform() {    	
+//    	String s = StdIn.readString(); // can use this for QA
     	String s = BinaryStdIn.readString();
-    	int[] t = new int[s.length()];
+    	char[] t = new char[s.length()];
     	CircularSuffixArray cs = new CircularSuffixArray(s);
     	for (int i = 0; i < s.length(); i++) {
-    		t[i] = s.charAt(cs.index(i));
+    		t[i] = lastCharInCircularSuffix(s, cs.index(i));
+    		BinaryStdOut.write(t[i]);
     	}
-    	BinaryStdOut.write(s);
-    	BinaryStdOut.close(); //closes output stream as per the API instructions
+    	BinaryStdOut.close();
+    }
+    
+    //based on the charAt method in CircularSuffic
+    private static char lastCharInCircularSuffix(String s, int index) {
+    	return s.charAt((index + (s.length() - 1)) % s.length()); // a bit tricky
     }
 
     // apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
@@ -24,7 +30,8 @@ public class BurrowsWheeler {
     // if args[0] is '+', apply Burrows-Wheeler inverse transform
     public static void main(String[] args) {
     	transform();
-    	
+//    	char c = lastCharInCircularSuffix("ABRACADABRA",3);
+//    	System.out.println(c);
     }
 
 }
